6c9d3f8bbd22131f2726ce6564e62ccac43e5e0c
Orr Goren <orr.goren@zoominfo.com>
2023-02-28 00:28:39 +0200

    upload

--- logic.py
+++ logic.py
@@ -100,13 +100,13 @@ class Yad2Logic:
             url = f"http://2captcha.com/res.php?key={api_key}&action=get&id={request_id}&json=1"
 
             status = 0
-            # while not status:
-            #     res = requests.get(url)
-            #     if res.json()['status'] == 0:
-            #         time.sleep(3)
-            #     else:
-            #         requ = res.json()['request']
-            #         print(requ)
+            while not status:
+                res = requests.get(url)
+                if res.json()['status'] == 0:
+                    time.sleep(3)
+                else:
+                    requ = res.json()['request']
+                    print(requ)
 
         return parsed_html
 
