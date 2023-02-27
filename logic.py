b0026831f5d1420992b31c86a66091fa9993c471
Orr Goren <orr.goren@zoominfo.com>
2023-02-28 00:49:34 +0200

    upload

--- logic.py
+++ logic.py
@@ -97,12 +97,12 @@ class Yad2Logic:
             print(response.content)
             request_id = response.json()['request']
 
-            url = f"http://2captcha.com/res.php?key={api_key}&action=get&id={request_id}&json=1"
+            get_url = f"http://2captcha.com/res.php?key={api_key}&action=get&id={request_id}&json=1"
 
             status = 0
             data = {}
             while not status:
-                res = requests.get(url)
+                res = requests.get(get_url)
                 if res.json()['status'] == 0:
                     time.sleep(3)
                 else:
